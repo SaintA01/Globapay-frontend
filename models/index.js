802da4c614ae504f02bf5b20cf1d38ebff82d293
SaintA01 <exceladebayo112@gmail.com>
2025-11-02 05:16:49 +0100

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,11 @@
+// Export all models
+module.exports = {
+  User: require('./User'),
+  Country: require('./Country'),
+  Service: require('./Service'),
+  Network: require('./Network'),
+  Plan: require('./Plan'),
+  Transaction: require('./Transaction'),
+  ApiProvider: require('./ApiProvider'),
+  Payment: require('./Payment')
+};
